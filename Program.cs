508e9fbb42592589ad7dedb88928756d6be353ce
patrick alexandre <palexandre@live.com>
2022-05-02 19:55:25 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace sistema_que_diz_o_primeiro_e_ultimo_nome_do_usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string PrimeiroNome;
+            string SegundoNome;
+            string TerceiroNome;
+
+            Console.WriteLine("Digite seu primeiro nome");
+            PrimeiroNome = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Digite seu segundo nome");
+            SegundoNome = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Digite seu terceiro nome");
+            TerceiroNome = Convert.ToString(Console.ReadLine());
+            
+            Console.WriteLine("Seu primeiro nome é: " + PrimeiroNome);
+            Console.WriteLine("Seu ultimo nome é: " + TerceiroNome);
+
+           
+        }
+    }
+}
